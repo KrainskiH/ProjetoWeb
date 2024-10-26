c71cb9654c4978b76d6657be7848c83a1b7d2cf8
Krainski <henriquekrainski38@gmail.com>
2024-10-26 14:41:00 -0300

    Update cadastro.js

--- cadastro.js
+++ cadastro.js
@@ -1,28 +1,32 @@
 function enviarDados() {
-    // Obtém os valores dos campos
-    const nome = document.getElementById("nome").value;
-    const data = document.getElementById("data").value;
-    const CPF = document.getElementById("CPF").value;
-    const email = document.getElementById("email").value;
-    const number = document.getElementById("number").value;
+        // Obtém os valores dos campos
+        const nome = document.getElementById("nome").value;
+        const data = document.getElementById("data").value;
+        const CPF = document.getElementById("CPF").value;
+        const email = document.getElementById("email").value;
+        const number = document.getElementById("number").value;
+        const senha = document.getElementById("senha").value;
 
+        // Verifica se todos os campos estão preenchidos
+        if (nome && data && CPF && email && number && senha) {
+            // Cria um objeto para armazenar os dados
+            const usuario = {
+                nome: nome,
+                data: data,
+                CPF: CPF,
+                email: email,
+                number: number,
+                senha: senha
+            };
 
-    // Verifica se todos os campos estão preenchidos
-    if (nome && data && CPF && email && number) {
-        // Cria uma nova linha na tabela
-        const table = document.getElementById("dadosTable").getElementsByTagName('thead')[0];
-        const newRow = table.insertRow();
+            // Armazena os dados no LocalStorage
+            localStorage.setItem(nome, JSON.stringify(usuario));
 
-        // Insere os dados nas células
-        newRow.insertCell(0).innerText = nome;
-        newRow.insertCell(1).innerText = data;
-        newRow.insertCell(2).innerText = CPF;
-        newRow.insertCell(3).innerText = email;
-        newRow.insertCell(4).innerText = number;
+            // Limpa o formulário
+            document.getElementById("cadastroForm").reset();
+            alert("Cadastro realizado com sucesso!");
 
-        // Limpa o formulário
-        document.getElementById("cadastroForm").reset();
-    } else {
-        alert("Por favor, preencha todos os campos.");
+        } else {
+            alert("Por favor, preencha todos os campos.");
+        }
     }
-}
