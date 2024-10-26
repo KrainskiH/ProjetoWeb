6a3590fb93a252fa70325413a377392bc1e16a48
CarlaPrislanny <prislannysilva@gmail.com>
2024-10-26 18:59:43 -0300

    Update saude.js
    Finalizado

--- saude.js
+++ saude.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let saudeMental = document.getElementById('saudeMental');
     let higiene = document.getElementById('higiene');
 
-    let editIndex = -1; // Índice para edição de um item
+    let editIndex = -1; // Índice para edição de um item 
 
     document.getElementById('cadastroForm').addEventListener('submit', function (e) {
         e.preventDefault();
